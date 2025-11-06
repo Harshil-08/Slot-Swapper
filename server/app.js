b8d11f3a2149861a033944f2e1099deee27c2875
Harshil Tanwar <harshiltanwar62@gmail.com>
2025-11-06 09:38:23 +0530

    fix: CORS

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "http://localhost:3000"],
+		origin: ["http://localhost:5173", "http://localhost:3000","https://slot-swapper-sv2a.onrender.com"],
 		credentials: true,
 	}),
 );
--- server/socket.js
+++ server/socket.js
@@ -7,7 +7,7 @@ let io;
 export const handleWebsocket = (server) => {
 	io = new Server(server, {
 		cors: {
-			origin: ["http://localhost:5173", "http://localhost:3000"],
+			origin: ["http://localhost:5173", "http://localhost:3000","https://slot-swapper-sv2a.onrender.com"],
 			credentials: true,
 		},
 	});
